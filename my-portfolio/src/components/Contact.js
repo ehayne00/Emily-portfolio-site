0b9b780442e1cc89147b579479c7feab593b3fdf
github username <github email address>
2020-03-18 23:26:44 +0000

    update

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,11 +1,38 @@
-import React from "react"
+import React from "react";
+import map from "../media/north-london-map.png";
+import linkedin from "../media/Linkedin-icon.png";
+import medium from "../media/Medium-icon.png";
+import github from "../media/Github-icon.png";
 
 const Contact = () => {
-    return(
-        <div>
-
+  return (
+    <div>
+      <h2 className="Header text-5">CONTACT</h2>
+      <div className="contact-page1">
+        <div className="contact-page2">
+          <a href="https://linkedin.com/in/emily-violet-haynes-519279182">
+            <img className="icon-size squarish" alt="oh no!" src={linkedin} />
+          </a>
+          <a href="https://medium.com/@ehayne00">
+            <img className="icon-size rounded" alt="oh no!" src={medium} />
+          </a>
+          <a href="https://github.com/ehayne00">
+            <img className="icon-size rounded" alt="oh no!" src={github} />
+          </a>
         </div>
-    )
-}
+        <img className="map-size" alt="oh no!" src={map} />
+      </div>
+      <div className="contact-text">
+        <h3 className="text-2">ehayne00@hotmail.com</h3>
+        <p className="text-1">
+          <b>
+            Born and raised in the London borough of Haringey, North-London. I
+            drive as well as utilising public transport.
+          </b>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
--- my-portfolio/src/components/ProjectCard.js
+++ my-portfolio/src/components/ProjectCard.js
@@ -3,14 +3,14 @@ import React from "react";
 const ProjectCard = ({ project }) => {
   return (
     <div className="card-space">
-      <h1>{project.title}</h1>
+      <h1 className="text-3"><u>{project.title}</u> - {project.date}</h1>
       <video className="video-size" controls>
         <source src={project.demo} type="video/mp4" />
       </video>
-      <div className="card-width">
-        <h1>{project.description}</h1>
-        <h1>{project.languages}</h1>
-        <h1>{project.inspiration}</h1>
+      <div className="card-width text-1">
+        <h1>DESCRIPTION: {project.description}</h1>
+        <h1>LANGUAGES/TECHNOLOGIES: {project.languages}</h1>
+        <h1>INSPIRATION: {project.inspiration}</h1>
       </div>
     </div>
   );
