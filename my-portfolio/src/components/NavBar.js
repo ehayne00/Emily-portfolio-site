53d9dbe899815d8b12d91786d1971d215e8fe5e5
github username <github email address>
2020-03-24 21:30:56 +0000

    update

--- my-portfolio/src/components/NavBar.js
+++ my-portfolio/src/components/NavBar.js
@@ -1,17 +1,37 @@
 import React from "react"
 import {NavLink} from 'react-router-dom'
 import photo from '../media/MyPhoto.JPG'
+import menuicon from '../media/menu.svg'
 
-const NavBar = () => {
+
+class NavBar extends React.Component {
+
+    state = {
+        sidebarShowing: false
+    }
+
+    toggleSidebarShowing = () => {
+        this.setState({
+            sidebarShowing: !this.state.sidebarShowing
+        })
+    }
+
+    render () {
     return (
         <div className="Nav">
             <h2 className="title-width text-2">Emily Violet Haynes Portfolio</h2>
+            <p className="text-2 software-position">Software Engineer</p>
             <img className="my-photo" alt="Oh no!" src={photo}/>
-            <NavLink className="Navlink-position" to='/' exact><h2 className="Navlink-size text-1">About Me</h2></NavLink>
-            <NavLink className="Navlink-position" to='/projects' exact><h2 className="Navlink-size text-1">My Projects</h2></NavLink>
-            <NavLink className="Navlink-position" to='/contact' exact><h2 className="Navlink-size text-1">Contact</h2></NavLink>
+            <img onClick={this.toggleSidebarShowing}className="menuicon" alt="oh no!" src={menuicon}/>
+            {this.state.sidebarShowing && (
+            <div className="nav-links">
+            <h2 onClick={this.toggleSidebarShowing} className="Navlink-size text-2">-<NavLink className="Navlink-position" to='/' exact>About Me</NavLink></h2>
+            <h2 onClick={this.toggleSidebarShowing}className="Navlink-size text-2">-<NavLink className="Navlink-position" to='/projects' exact>My Projects</NavLink></h2>
+            <h2 onClick={this.toggleSidebarShowing}className="Navlink-size text-2">-<NavLink className="Navlink-position" to='/contact' exact>Contact</NavLink></h2>
+            </div>)}
         </div>
     )
+    }
 }
 
 export default NavBar;
