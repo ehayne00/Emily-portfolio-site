1a7c009742d9a4a40ad7be9a3fa9906946597adf
github username <github email address>
2020-03-29 16:22:34 +0100

    youtube

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,15 +3,27 @@ import React from "react";
 const ProjectCard = ({ project }) => {
   return (
     <div className="card-space">
-      
-      <p className="text-3 about-me-padding project-title"><u>{project.title}</u> - {project.date}</p>
-      
-      <video className="video-size" controls>
+      <p className="text-3 about-me-padding project-title">
+        <u>{project.title}</u> - {project.date}
+      </p>
+
+      {/* <video className="video-size" controls>
         <source src={project.demo} type="video/mp4" />
-      </video>
+      </video> */}
+
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/Fh0PEm7sW94"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
       <div className="card-width text-1">
         <p className="project-text">DESCRIPTION: {project.description}</p>
-        <p className="project-text">LANGUAGES/TECHNOLOGIES: {project.languages}</p>
+        <p className="project-text">
+          LANGUAGES/TECHNOLOGIES: {project.languages}
+        </p>
         <p className="project-text">INSPIRATION: {project.inspiration}</p>
       </div>
     </div>
