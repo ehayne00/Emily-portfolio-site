8830ff65ef9a48a4df2006d58acee7ebdad91535
ehayne00 <ehayne00@hotmail.com>
2020-03-31 17:04:21 +0100

    updated json

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ export default [
       date: "March 2020",
       demo: "https://www.youtube.com/embed/Fh0PEm7sW94",
       description: "A platform for surfers to share their awesome pics/vids and global surf experiences, stories and recommendations.",
-      languages: "React.js, Ruby on Rails as a restful API, Google API's (maps, places autocomplete and cloud storage), Active Storage, React Router library, Media queries to ensure multiple mobile phone screen dimention adjustment.",
+      languages: "React.js, ES6 syntax, Ruby on Rails as a restful API, Google API's (maps, places autocomplete and cloud storage), Active Storage, React Router library, Media queries to ensure multiple mobile phone screen dimention adjustment, CSS, HTML5/JSX.",
       inspiration: "I am a keen traveller, who also loves anything surf, from the activity, to the general vibe that comes with it. This site is something I would use myself, to search for inspiration for a holiday destination to choose next, based on where the best surf spots/recommendations are."
     },
     {
@@ -14,7 +14,7 @@ export default [
       date: "February 2020",
       demo: "https://www.youtube.com/embed/fk3tastsYyY",
       description: "A stylish e-commerce site for the sale of clothes for women, men and kids, crafted by both myself and Hiroshi Sipahioglu, a fellow Flatiron School graduate.",
-      languages: "React.js, Ruby on Rails as a restful API, React Router library.",
+      languages: "React.js, ES6 syntax, Ruby on Rails as a restful API, React Router library, CSS, HTML5/JSX.",
       inspiration: "I regularly use clothes shopping sites myself and wanted to try my hand at making one."
     },
     {
@@ -23,7 +23,7 @@ export default [
       date: "January 2020",
       demo: "https://www.youtube.com/embed/tGUsSCYKi2o",
       description: "A game where one must type the correct words in order for Happy Harry to survive.",
-      languages: "Vanilla JavaScript and Ruby on Rails as a restful API.",
+      languages: "Vanilla JavaScript, Ruby on Rails as a restful API, CSS, HTML.",
       inspiration: "Just for fun!"
     }
   ];
