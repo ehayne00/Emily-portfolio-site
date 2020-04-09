67bceb257ccf56efa14b62549c26de660c6168eb
ehayne00 <ehayne00@hotmail.com>
2020-04-09 15:11:18 +0100

    added new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,29 +1,47 @@
 export default [
-    {
-      id: 28,
-      title: "Wax Global",
-      date: "March 2020",
-      demo: "https://www.youtube.com/embed/Fh0PEm7sW94",
-      description: "A platform for surfers to share their awesome pics/vids and global surf experiences, stories and recommendations.",
-      languages: "React.js, ES6 syntax, Ruby on Rails as a restful API, Google API's (maps, places autocomplete and cloud storage), Active Storage, React Router library, Media queries to ensure multiple mobile phone screen dimention adjustment, CSS, HTML5/JSX, JWL for Auth.",
-      inspiration: "I am a keen traveller, who also loves anything surf, from the activity, to the general vibe that comes with it. This site is something I would use myself, to search for inspiration for a holiday destination to choose next, based on where the best surf spots/recommendations are."
-    },
-    {
-      id: 27,
-      title: "The Clothes Store",
-      date: "February 2020",
-      demo: "https://www.youtube.com/embed/fk3tastsYyY",
-      description: "A stylish e-commerce site for the sale of clothes for women, men and kids, crafted by both myself and Hiroshi Sipahioglu, a fellow Flatiron School graduate.",
-      languages: "React.js, ES6 syntax, Ruby on Rails as a restful API, React Router library, CSS, HTML5/JSX, JWL for Auth.",
-      inspiration: "I regularly use clothes shopping sites myself and wanted to try my hand at making one."
-    },
-    {
-      id: 26,
-      title: "Type To Survive",
-      date: "January 2020",
-      demo: "https://www.youtube.com/embed/tGUsSCYKi2o",
-      description: "A game where one must type the correct words in order for Happy Harry to survive.",
-      languages: "Vanilla JavaScript, Ruby on Rails as a restful API, CSS, HTML.",
-      inspiration: "Just for fun!"
-    }
-  ];
+  {
+    id: 29,
+    title: "Checkout Page Request",
+    date: "April 2020",
+    demo: "https://www.youtube.com/embed/6Ljn06o3EBw",
+    description:
+      "This demo video is a recorded response to a company who asked me to create a checkout/ account creation/ contract creation page, including a brief look into some of the code for it.",
+    languages:
+      "React.js with ES6 syntax, CSS, HTML5/JSX, and mocking how I would communicate with a server.",
+    inspiration: "Hometree Company",
+  },
+  {
+    id: 28,
+    title: "Wax Global",
+    date: "March 2020",
+    demo: "https://www.youtube.com/embed/Fh0PEm7sW94",
+    description:
+      "A platform for surfers to share their awesome pics/vids and global surf experiences, stories and recommendations.",
+    languages:
+      "React.js, ES6 syntax, Ruby on Rails as a restful API, Google API's (maps, places autocomplete and cloud storage), Active Storage, React Router library, Media queries to ensure multiple mobile phone screen dimention adjustment, CSS, HTML5/JSX, JWL for Auth.",
+    inspiration:
+      "I am a keen traveller, who also loves anything surf, from the activity, to the general vibe that comes with it. This site is something I would use myself, to search for inspiration for a holiday destination to choose next, based on where the best surf spots/recommendations are.",
+  },
+  {
+    id: 27,
+    title: "The Clothes Store",
+    date: "February 2020",
+    demo: "https://www.youtube.com/embed/fk3tastsYyY",
+    description:
+      "A stylish e-commerce site for the sale of clothes for women, men and kids, crafted by both myself and Hiroshi Sipahioglu, a fellow Flatiron School graduate.",
+    languages:
+      "React.js, ES6 syntax, Ruby on Rails as a restful API, React Router library, CSS, HTML5/JSX, JWL for Auth.",
+    inspiration:
+      "I regularly use clothes shopping sites myself and wanted to try my hand at making one.",
+  },
+  {
+    id: 26,
+    title: "Type To Survive",
+    date: "January 2020",
+    demo: "https://www.youtube.com/embed/tGUsSCYKi2o",
+    description:
+      "A game where one must type the correct words in order for Happy Harry to survive.",
+    languages: "Vanilla JavaScript, Ruby on Rails as a restful API, CSS, HTML.",
+    inspiration: "Just for fun!",
+  },
+];
